b8d5eb68689e52265e247a4f5b6a1bfbcb91b773
nandorcsupor-met <nandor@met.capital>
2024-12-31 18:56:30 +0100

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
@@ -8,9 +7,6 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src="/logo.png" className="logo react" alt="React logo" />
         </a>
